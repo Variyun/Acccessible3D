82df635543b7729ebe8a58a844a8d7754ded7f0b
Aaron <aaron.chen1@ucalgary.ca>
2021-03-01 19:32:00 -0700

    Added example landing page

---
+++ accessible3d/src/components/landing-page.vue
@@ -0,0 +1,37 @@
+<template>
+  <v-container height="100%" class="pa-0 ma-0" fluid>
+    <v-parallax id="back" src='../assets/background.png' height="1000">
+        <v-row class="text-right ma-0 pd-0"> <v-col> <p> READY TO GET STARTED? CLICK HERE </p> </v-col> </v-row> 
+        <v-row  align="center" justify="center" style="margin-bottom: 20%;" >
+            <v-col class="text-center">
+                <h1 class="display-4 font-weight-bold mb-5">
+                ACCESSIBLE 3D
+                </h1>
+                <p class="display-1 font-weight-bold">
+                Reconstruct Your World
+                </p>
+            </v-col>
+        </v-row>
+    </v-parallax>
+    <v-row align="left"> 
+      <v-col class="pl-10">
+        <h1 class="display-4 font-weight-bold mb-5">
+          LOREM IPSUM
+        </h1>
+        <p class="display-1 font-weight-bold" style="margin-right: 10%">
+          Lorem ipsum dolor sit amet, consectetur adipiscing elit. Sed lobortis erat purus, nec dictum mi molestie accumsan. Vestibulum convallis nunc erat, eget scelerisque tortor elementum quis. Maecenas semper lacus massa, nec molestie felis faucibus elementum. Mauris libero ex, commodo at est vitae, efficitur vehicula diam. Aliquam lacus velit, dignissim nec enim in, dictum gravida est. Suspendisse et ligula ac leo volutpat elementum. Fusce a odio metus. Maecenas volutpat, erat ac porttitor hendrerit, risus nibh efficitur augue, eu fermentum dui nisi sit amet lorem. Ut convallis metus vel ex sodales, sit amet mollis eros varius. Sed malesuada nisi sit amet finibus varius.
+        </p>
+      </v-col>
+    </v-row>
+  </v-container>
+</template>
+
+<script>
+  export default {
+    name: 'landing',
+
+    data: () => ({
+    //
+    }),
+  }
+</script>
